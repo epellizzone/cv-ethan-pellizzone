6c1233d8fa453142ecc9744e0f840da97d49ea8f
epellizzone <148949291+epellizzone@users.noreply.github.com>
2023-10-27 11:45:31 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <section class="container"
+            <div>
+                <img src="https://i.pinimg.com/originals/d4/37/4b/d4374b6dc2934880eaa7a5e8989c1f64.jpg" 
+                alt="image" heigth="100" width="100" />
+            </div>
+            <div>
+                <h3 class="info">INFO PERSONNELLES</h3>
+                <p>Nom: Ethan Pellizzone</p>
+                <p>adresse: 7 rue des claire fontaine</p>
+                <p>numéro de télephone: <a  href="tel:0587878787">0475/27/14/95</a></p> 
+                <p>E-mail:<a href="mailto:ethan:pellizzone@gmail.com"> ethan.pellizzone@gmail.com </a>
+                <p>Date de naissance:18 mars 2006</p>
+                <p>Lieu de naissance: charleroi</p>
+                <p>linkedin:<p><a href="https://www.linkedin.com/in/ethan-pellizzone-345b99296/">linkedin.com/ethan-pellizzone</a>
+            </div>
+            <div >
+                <h2 class="langue"> LANGUE</h2>
+                <ul>
+                <li> Français </li>
+                <div class="progress-container4">
+                    <div class="progress-bar4"></div>
+                </div>
+                <li class="anglais"> Anglais </li>
+                <div class="progress-container5">
+                    <div class="progress-bar5"></div>
+                </div>
+                </ul>
+            </div>
+            <div>
+                <H2 class="interet"> INTÉRÊTS </H2>
+                <p></p>
+                <ul>
+                <li>sport</li>
+                <li>lire</li>
+                <li></li>
+                </ul>
+            </div>
+        </section>
+        <section class="container2">
+            <div> <!-- présentation -->
+                <h1 class="ethan">Ethan Pellizzone</h1>
+                <p class="presentation">Bonjour, je m'appelle Ethan Pellizzone et j'ai 17 ans. Je suis actuellement en première année d'études en développement web. 
+                    J'ai acquis des bases  en anglais, ce qui me permet de naviguer aisément dans un environnement anglophone.
+                     Mon français est ma langue maternelle, et je le parle couramment. En ce qui concerne mes compétences en développement web,
+                      j'ai une bases de HTML et CSS, ce qui me permet de concevoir des pages web avec une mise en page attrayante et responsive.
+                       J'ai également quelques connaissances en PHP, bien que je sois encore en train de les développer. En ce qui concerne JavaScript,
+                        je suis encore débutant, mais je suis enthousiaste à l'idée d'apprendre davantage et de devenir plus compétent dans ce langage essentiel pour le développement web.</p> 
+                <p></p>
+            </div>
+            <!-- enseignement --->
+            <div> 
+                <h2 class="enseignement">ENSEIGNEMENT</h2>
+                <div>
+                <p>sept2019-juin222</p> 
+                <p class="charleroi">Institut Jean Jaurès Charleroi</p>
+                </div>
+                <div>
+                   <p></p>
+                   <p></p>                
+                </div>
+            </div>
+            <div> <!--experience profesionnelle-->
+                <h2 class="experience">EXPÉRIENCE PERSONNELLE</h2> 
+                <div>
+                    <p class="aucune">aucune</p>
+                </div>
+                <div>
+                    <p></p>
+                </div>
+            </div>
+            <div> <!--compétences-->
+            <h2 class="competences">COMPÉTENCES</h2> 
+                <p class="html">HTML5 </p>
+                <div class="progress-container">
+                    <div class="progress-bar"></div>
+                </div>
+                <p>CSS3</p>
+                <div class="progress-container1">
+                    <div class="progress-bar1"></div>
+                </div>
+                <p>PHP</p>
+                <div class="progress-container2">
+                    <div class="progress-bar2"></div>
+                </div>
+                <p class="JS">JS<p>  
+                <div class="progress-container3">
+                    <div class="progress-bar3"></div>
+                </div>              
+               </div>
+               <div>
+               <p></p>
+               <p></p>
+               <p></p>
+               <p><p>
+               </div>            
+            </div>
+            <div> <!--références-->
+                <h2 class="references">RÉFÉRENCES</h2> 
+                 <p>M.Patrick Ghesquiere</p>                    
+                 <p><p>E-mail:<a href="mailto:patrick.ghesquiere@ifapme.be"> patrick.ghesquiere@ifapme.be </a></p>
+                 <p></p>                
+                </div>  
+                <div></div>                
+               
+           </div> 
+        </section>
+    </main>
+</body>
+</html>
